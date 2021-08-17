8b6403d0555df9356555ef8326618ac7636f64dc
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-17 04:12:01 +0300

    feat: create project UIButtonAndLabel on the topic "UIButton and UILabel"

---
+++ UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIButtonAndLabel
+//
+//  Created by Viktoriia Jane Ro on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
