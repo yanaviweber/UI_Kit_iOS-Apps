abfee8db2221ebfa46a30e329428fa4816aaf8a1
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-17 22:53:02 +0300

    feat: create connections with buttons and their outlets; Create IBAction statements for the buttons; Success test it in the simulator

--- UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
+++ UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
@@ -14,33 +14,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     
+    @IBOutlet var actionButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         label.isHidden = true
         label.font = label.font.withSize(35)
-        label.textColor = UIColor.red
         
-        button.setTitle("Get result", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.backgroundColor = .green
+        button.isHidden = true
+        
+        for button in actionButtons {
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+        
+        button.setTitle("Clear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
         
     }
 
     @IBAction func pressedButton(_ sender: UIButton) {
         
-        if label.isHidden {
-            label.isHidden = false
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.tag == 0 {
             label.text = "Hello, World!"
-            
-            button.setTitle("Clear", for: UIControl.State.normal)
-            button.setTitleColor(.white, for: .normal)
-            button.backgroundColor = .red
-        } else {
+            label.textColor = .red
+        } else if sender.tag == 1 {
+            label.text = "Hi, there!"
+            label.textColor = .blue
+        } else if sender.tag == 2 {
             label.isHidden = true
-            button.setTitle("Get result", for: .normal)
-            button.setTitleColor(.blue, for: .normal)
-            button.backgroundColor = .green
+            button.isHidden = true
         }
         
     }
