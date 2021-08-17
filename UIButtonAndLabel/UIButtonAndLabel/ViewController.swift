9599349a2b45ba6691a553d93ac3b2344f5264af
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-17 07:40:46 +0300

    fix: edit parameters of the button by the code: title, font, background color, actions, results after click button and before click button

--- UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
+++ UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
@@ -21,11 +21,28 @@ class ViewController: UIViewController {
         label.font = label.font.withSize(35)
         label.textColor = UIColor.red
         
+        button.setTitle("Get result", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .green
+        
     }
 
     @IBAction func pressedButton(_ sender: UIButton) {
         
-        label.text = "Hello, World!"
+        if label.isHidden {
+            label.isHidden = false
+            label.text = "Hello, World!"
+            
+            button.setTitle("Clear", for: UIControl.State.normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .red
+        } else {
+            label.isHidden = true
+            button.setTitle("Get result", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+        
     }
     
 }
