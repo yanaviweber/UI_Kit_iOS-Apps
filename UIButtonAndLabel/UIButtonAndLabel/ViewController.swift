bcb5bd1156bb3b27cd104bca81f69e72b0a876f6
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-17 07:20:52 +0300

    feat: set connections with controller and elements; edit constraints for elements in the Portrait view(iPhone, iPod) to correctly everywhere

--- UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
+++ UIButtonAndLabel/UIButtonAndLabel/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressedButton(_ sender: UIButton) {
+    }
+    
 }
 
