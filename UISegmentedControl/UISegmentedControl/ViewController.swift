0bc48a74ab2eea37111dbce8e4eb2c3442922e4b
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-18 01:23:08 +0300

    feat: set connections with elements in interface and code in the ViewController(set IBOutlets and IBActions for this)

--- UISegmentedControl/UISegmentedControl/ViewController.swift
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+    }
+    
 }
 
