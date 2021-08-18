e12f10755255674ce5da09528a42e98d2b5fbd42
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-18 03:11:57 +0300

    feat: add connections of the Outlets and Actions for the segmented control and label; Set functionality actions and appearance to them; Success test it in the simulator

--- UIElements/UIElements/ViewController.swift
+++ UIElements/UIElements/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
     }
 
-
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The first segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The second segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The third segment is selected"
+            label.textColor = .yellow
+        default:
+            print("Something is wrong!")
+        }
+        
+    }
+    
 }
-
