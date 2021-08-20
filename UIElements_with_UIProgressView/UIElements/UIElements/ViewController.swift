0c1575918e52482a19469d70c73c45263625e969
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-21 00:15:29 +0300

    feat: set new appearance parameter for the UITextView in the code; Set background for the editing text mode and corner radius of the area

--- UIElements_with_UIProgressView/UIElements/UIElements/ViewController.swift
+++ UIElements_with_UIProgressView/UIElements/UIElements/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        textView.delegate = self
+        
+        textView.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 17)
+        textView.backgroundColor = self.view.backgroundColor
+        textView.layer.cornerRadius = 10
+        
     }
 
 
 }
 
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = .white
+        textView.textColor = .gray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = self.view.backgroundColor
+        textView.textColor = .black
+    }
+    
+}
