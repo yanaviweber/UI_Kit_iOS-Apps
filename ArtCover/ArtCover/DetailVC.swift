d5ae30addcd47bf090f5a533f45ef6d12f88f47e
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-24 00:43:23 +0300

    feat: add appearance parameters to output data after click on the music song by code; Success test it in the simulator

--- ArtCover/ArtCover/DetailVC.swift
+++ ArtCover/ArtCover/DetailVC.swift
@@ -13,21 +13,14 @@ class DetailVC: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    var trackTitle = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        image.image = UIImage(named: trackTitle)
+        titleLabel.text = trackTitle
+        titleLabel.numberOfLines = 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
