a6fe86cacb2b1ae58800ac9deed77813c313d8a7
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-26 21:28:59 +0300

    start: create files of project "BooksLister"

---
+++ BooksLister/BooksLister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BooksLister
+//
+//  Created by Viktoriia Jane Ro on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
