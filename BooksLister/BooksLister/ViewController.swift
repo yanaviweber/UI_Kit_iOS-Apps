5fd5f486dbde97ee247a0712c70c44aa2629d176
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-26 22:16:42 +0300

    feat: add new connections (IBOutlets) for the elements in the screen; Add new class for the TableViewController(DetailViewController) and create file of this class; Add new Identifier "Title" for the UITableView Cell in the Identity Inspector

--- BooksLister/BooksLister/ViewController.swift
+++ BooksLister/BooksLister/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOfTheBook: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
