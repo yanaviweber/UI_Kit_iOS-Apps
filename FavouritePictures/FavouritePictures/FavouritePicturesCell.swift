b7e5a6cb6aca95aab0fb8322547169aca0aa2f70
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-29 07:40:34 +0300

    feat: add new class file for the UI CollectionViewCell - FavouritePicturesCell.swift

---
+++ FavouritePictures/FavouritePictures/FavouritePicturesCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavouritePicturesCell.swift
+//  FavouritePictures
+//
+//  Created by Viktoriia Jane Ro on 29.08.2021.
+//
+
+import UIKit
+
+class FavouritePicturesCell: UICollectionViewCell {
+    
+}
