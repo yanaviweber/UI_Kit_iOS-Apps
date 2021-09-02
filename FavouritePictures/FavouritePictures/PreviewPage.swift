0d861161bca3d253ead485ab19a79783808f0496
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-02 06:37:47 +0300

    fix: update functionality for output name of the image: current name of the image shows in the console of Xcode

--- FavouritePictures/FavouritePictures/PreviewPage.swift
+++ FavouritePictures/FavouritePictures/PreviewPage.swift
@@ -54,6 +54,12 @@ class PreviewPage: UICollectionViewController {
         let pictureImage = UIImage(named: imageName)
         cell.favouritePicturesImageView.image = pictureImage
 
+        // 02092021
+        let titleImage = imageNameArray[indexPath.row]
+        cell.favouritePicturesImageView.image = UIImage(named: titleImage)
+        // 02092021
+        
+        
         // -- еще одна поытка вывести название картинки
         //let imageTitle = imageNameArray[indexPath.item]
         //let titleSection = UIImage(named: imageTitle)
@@ -74,21 +80,32 @@ class PreviewPage: UICollectionViewController {
             
             //showPictureVC.pictureTitle = cell.favouritePicturesImageView.image?.description  --еще одна попытка вывести название
             
-            if let indexPath = self.collectionView.indexPathsForSelectedItems {
+            //if let indexPath = self.collectionView.indexPathsForSelectedItems {
                 
-                // ------- не понимаю как взять заголовок с картинки и вывести его на втором скрине в ачестве названия картины --------
+                // ------- не понимаю как взять заголовок с картинки и вывести его на втором скрине в качестве названия картины --------
                 
                 //let imageTitle = showPictureVC.pictureTitle
                 //showPictureVC.pictureTitle = imageTitle
                 
-                let imageTitle = imageNameArray[indexPath.count - 1]
+                //let imageTitle = imageNameArray[indexPath.count - 1]
                 // let imageTitle = imageNameArray[indexPath.item]
-                 showPictureVC.pictureTitle = imageTitle
+                //showPictureVC.pictureTitle = imageTitle
                 
                 //showPictureVC.titleLabelShowPictureVC = imageNameArray[indexPath.item]
-            }
+            //}
             
         }
     }
     
+    // 02092021 -- сейчас в даннный момент выводит заголовок текущей выбранной картины в консоль Xcode.Выводит не рандомное название а именно выбранной картины. НО в консоли.
+    override public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+       let imageNameSelected = imageNameArray[indexPath.row]
+        
+       print(imageNameSelected)
+    }
+    // 02092021
+    
+    
+    //--- Нужно все-таки сделать так чтобы оно наконец-то вывело заголовок на втором екране под текущей выбранной картинкой и имено от той картинки которая выбрана на екране (не рандомное название); -----//
 }
