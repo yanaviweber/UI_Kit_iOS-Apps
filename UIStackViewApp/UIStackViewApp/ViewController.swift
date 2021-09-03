dcdffd6f890f2f66d22dcb14c4849af65e2c55ce
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-03 07:26:35 +0300

    start: create files of the project "UIStackViewApp"

---
+++ UIStackViewApp/UIStackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIStackViewApp
+//
+//  Created by Viktoriia Jane Ro on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
