f2a3aa059fac6db703ce6a378b61aa66fed0d62e
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-03 03:04:18 +0300

    fix: refactoring; Add new parameter for output text in the UILabel of the click on the image; Solved: Text of current image show in the detail image screen; Update image gallery for correct display mode; Success test it in the simulator

--- FavouritePictures/FavouritePictures/FavouritePicturesCell.swift
+++ FavouritePictures/FavouritePictures/FavouritePicturesCell.swift
@@ -10,4 +10,9 @@ import UIKit
 class FavouritePicturesCell: UICollectionViewCell {
     
     @IBOutlet weak var favouritePicturesImageView: UIImageView!
+    
+    // 03092021 --- предоставление места для заголовка картины. Но при этом его не видно на превью. Он отображается только на скрине деталей выбранной картины.
+    var pictureTitle = ""
+    // 03092021
+    
 }
