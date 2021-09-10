d2050b64dc5a5b081ef53a9f71c314c58ee50b51
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-10 21:38:17 +0300

    feat: add new elements: - UIButtons "Back" and "Forward", UITextfield - "urlTextField"; Add new constraints and IBOutlets and IBActions for this elements in the code

--- AwesomeBrowser/AwesomeBrowser/ViewController.swift
+++ AwesomeBrowser/AwesomeBrowser/ViewController.swift
@@ -12,8 +12,9 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
-    
-    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +26,12 @@ class ViewController: UIViewController {
         webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    @IBAction func backButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func forwardButtonAction(_ sender: Any) {
+    }
+    
+    
 }
 
