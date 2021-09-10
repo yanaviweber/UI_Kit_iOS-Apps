c7e56a942165ba8a966fced507cfc02848ea1bd5
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-10 21:16:36 +0300

    feat: add new element - WebView; Add constraints and add new connection in the IBOutlet for this element

--- AwesomeBrowser/AwesomeBrowser/ViewController.swift
+++ AwesomeBrowser/AwesomeBrowser/ViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
