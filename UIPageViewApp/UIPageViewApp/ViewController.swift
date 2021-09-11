0495fe44a87fcfd31eb8e41569c3f0fc9460dfe7
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-11 19:04:13 +0300

    start: create files of the project "UIPageViewApp"

---
+++ UIPageViewApp/UIPageViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPageViewApp
+//
+//  Created by Viktoriia Jane Ro on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
