e25657e68ddfbbf93b5635593c38fc240e947046
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-11 19:43:16 +0300

    feat: add new class files for the UI Page View Controller and UI Content View Controller; Set class parameter and Storyboard ID in the Identity Inspector

---
+++ UIPageViewApp/UIPageViewApp/ContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewController.swift
+//  UIPageViewApp
+//
+//  Created by Viktoriia Jane Ro on 11.09.2021.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
