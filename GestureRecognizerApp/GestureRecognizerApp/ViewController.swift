381862fe71979c55a26b2092258bd7a8dd57c2d9
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-13 23:35:49 +0300

    feat: add new functionality, - observers for our swipes; Add new function "handleSwipes" for our swipes for realization implementation logic

--- GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        swipesObservers()
     }
 
+    func swipesObservers() {
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
 
+    @objc func handleSwipes(gester: UISwipeGestureRecognizer) {
+        
+        switch gester.direction {
+        case .right:
+            label.text = "Right swipe was recognized"
+        case .left:
+            label.text = "Left swipe was recognized"
+        case .up:
+            label.text = "Up swipe was recognized"
+        case .down:
+            label.text = "Down swipe was recognized"
+        default:
+            break
+        }
+        
+    }
+    
+    
 }
 
