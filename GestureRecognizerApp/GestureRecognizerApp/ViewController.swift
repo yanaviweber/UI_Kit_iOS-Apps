031ac475e7e3d16a0adf811e1beccc2b836bc49c
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-09-13 23:10:27 +0300

    start: create files of project "GestureRecognizerApp"

---
+++ GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Viktoriia Jane Ro on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
